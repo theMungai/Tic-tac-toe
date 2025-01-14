231f1b10b937268e292cf09b28b881ea78ff2da3
theMungai <mungaithesimon@gmail.com>
2025-01-14 07:49:10 +0300

    Addeed Restart Button

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const gridCells = document.querySelectorAll(".grid-cell");
 const statusText = document.querySelector(".statusText");
-const restartBtn = document.querySelector(".restart-btn");
+const restartBtn = document.querySelector(".button-3d");
+const winConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [0,4,8]
+]
